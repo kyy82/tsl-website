2682b8e6e7127080ffb948feb2a31c0d1d01d9f7
Panagiotis Angeloudis <p@ngl.gr>
2025-03-17 14:00:18 +0000

    fix

--- view.sh
+++ view.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 #
-# Transport Systems Lab - Hugo View Scrip 
+# Transport Systems Lab - Jekyll View Script with temp directory
+
+# Create a temporary directory for the build
+TEMP_DIR=$(mktemp -d)
+echo "Building site to temporary directory: $TEMP_DIR"
+
+# Clean up the temporary directory on exit
+trap "rm -rf $TEMP_DIR" EXIT
 
 # Checks whether we are running on MacOS
 if [ "$(uname)" == "Darwin" ]; then
-	bundle exec jekyll serve --open-url --livereload --trace
+	bundle exec jekyll serve --destination $TEMP_DIR --open-url --livereload --trace
 # Assumes that we are running under the Windows Subsystem for Linux
 else 
-	bundle exec jekyll serve --open-url --livereload --trace
+	bundle exec jekyll serve --destination $TEMP_DIR --open-url --livereload --trace
 fi
